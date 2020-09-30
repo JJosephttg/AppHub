a921d9531a8f411c12703b7f81c809e3877f0487
TechyPRO <jjosephttg@gmail.com>
2020-09-30 18:05:55 -0400

    Added Initial Schema

--- public/DatabaseUtility.js
+++ public/DatabaseUtility.js
@@ -1,14 +1,10 @@
 const sqlite = require('sqlite3');
 const electron = require('electron');
 const { dialog, BrowserWindow } = electron;
-const fs = require('fs');
 
 let database;
 
 const open = () => {
-    let dbExists = false;
-    if(fs.existsSync("db.sqlite")) dbExists = true;
-
     database = new sqlite.Database("db.sqlite", (err) => {
         if(err) {
             dialog.showMessageBoxSync({
@@ -19,20 +15,25 @@ const open = () => {
         }
     });
 
-    /*let sql = 
-    `CREATE TABLE "Categories" (
+    let tableSchema = 
+    `CREATE TABLE IF NOT EXISTS 'Categories' (
         "id"	INTEGER NOT NULL UNIQUE,
         "CategoryName"	TEXT NOT NULL UNIQUE,
         PRIMARY KEY("id" AUTOINCREMENT)
-    );
-    CREATE TABLE "Apps"`;
+    ); CREATE TABLE IF NOT EXISTS 'Apps' (
+        "id"	INTEGER UNIQUE,
+        "AppName"	TEXT NOT NULL,
+        "Category"	INTEGER,
+        FOREIGN KEY("Category") REFERENCES "Categories"("id"),
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );`;
 
-    database.run(sql, (result, error) => {
+    database.exec(tableSchema, error => {
         if(error) dialog.showMessageBoxSync({
             title: "Database Error", 
             message: `Unable to create database schema: ${error}`
         });
-    });*/
+    });
 }
 
 const close = () => database.close();
