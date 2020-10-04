4753d90e0697aca6ff792f486b2aae589e4a78bb
TechyPRO <jjosephttg@gmail.com>
2020-10-04 15:40:45 -0400

    Added Working ToolTip to Page Header

--- src/Containers/ListingToolBar/ListingToolBar.js
+++ src/Containers/ListingToolBar/ListingToolBar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import ToolTip from '@material-ui/core/Tooltip'
 
 import styles from './ListingToolBar.module.css';
 
 const ListingToolBar = props => {
+    const [ shouldToolTip, setShouldToolTip] = useState(false);
+    const textElementRef = useRef();
+
+    const compareSize = () => {
+        const isTruncated = textElementRef.current.scrollWidth > textElementRef.current.clientWidth;
+        setShouldToolTip(isTruncated);
+    };
+
+    useEffect(() => {
+        compareSize();
+        window.addEventListener('resize', compareSize);
+
+        return () => window.removeEventListener('resize', compareSize);
+    }, []);
+
     return (
         <div className={styles["toolbar-container"]}>  
             <div className={[styles["left-container"], styles.container].join(" ")}>
@@ -14,8 +30,9 @@ const ListingToolBar = props => {
                         </svg>
                     </Link>
                 </div>
-                
-                <h1 className={styles["page-title"]}>Categories</h1>
+                <ToolTip title="Categories" placement="bottom" disableHoverListener={!shouldToolTip}>
+                    <h1 ref={textElementRef} className={styles["page-title"]}>Categories</h1>
+                </ToolTip>
             </div>
             <div className={[styles["middle-container"], styles.container].join(" ")}>
                 <div className={styles["search-logo"]}>
@@ -29,14 +46,14 @@ const ListingToolBar = props => {
             <div className={[styles["right-container"], styles.container].join(" ")}>
                 <button className={["btn btn-primary", styles.iconSizeFont].join(" ")}>+</button>
                 <button className={["btn btn-secondary", styles.iconSizeFont].join(" ")}>
-                <svg width=".7em" height=".7em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                <svg width=".7em" height=".7em" viewBox="0 0 16 16" className="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                 </svg>
                 </button>
                 <button className={["btn btn-danger", styles.iconSizeFont].join(" ")}>
-                    <svg width=".7em" height=".7em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <svg width=".7em" height=".7em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                     </svg>
                 </button>
             </div>            
