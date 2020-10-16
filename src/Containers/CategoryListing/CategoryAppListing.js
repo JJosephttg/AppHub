556af5cef247c0448cd751f736f32e83e74ea0aa
TechyPRO <jjosephttg@gmail.com>
2020-10-16 19:25:19 -0400

    Added App Hover Overlay

--- src/Containers/CategoryListing/CategoryAppListing.js
+++ src/Containers/CategoryListing/CategoryAppListing.js
@@ -12,6 +12,13 @@ const CategoryAppListing = props => {
                 {props.appList ? props.appList.map(app => (
                     <div key={app.id} className={["card", styles["app-container"]].join(" ")}>
                         <div className={[styles["app-overlay-container"], "card-body"].join(" ")}>
+                            <div className={styles["overlay"]}>
+                                <div className={styles["overlay-button"]}>
+                                    <svg viewBox="0 0 16 16" className="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                                    </svg>
+                                </div>
+                            </div>
                             {app.ImgSrc ? <img src={app.ImgSrc} /> : (
                                 <svg className={[styles["app-img"], "bi bi-window"].join(" ")} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd" d="M14 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"/>
