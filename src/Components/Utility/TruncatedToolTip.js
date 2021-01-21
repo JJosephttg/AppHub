b92e17adb06914760a7275ebd26c24095e65603b
Josh Joseph <jjosephttg@gmail.com>
2021-01-20 21:39:23 -0500

    Fixed Dependency warning

--- src/Components/Utility/TruncatedToolTip.js
+++ src/Components/Utility/TruncatedToolTip.js
@@ -1,21 +1,21 @@
-import React, { forwardRef, useState, useEffect } from 'react';
+import React, { forwardRef, useState, useEffect, useCallback } from 'react';
 import ToolTip from '@material-ui/core/Tooltip';
 
 const TruncatedToolTip = forwardRef((props, childRef) => {
     const [shouldToolTip, setShouldToolTip] = useState(false);
 
-    const compareSize = () => {
+    const compareSize = useCallback(() => {
         if(!childRef.current) return;
         const isTruncated = childRef.current.scrollWidth > childRef.current.offsetWidth;
         setShouldToolTip(isTruncated);
-    };
+    }, [childRef]);
 
     useEffect(() => {
         compareSize();
         window.addEventListener('resize', compareSize);
 
         return () => window.removeEventListener('resize', compareSize);
-    }, []);
+    }, [compareSize]);
 
     return (
         <ToolTip title={props.label ?? ""} placement={props.placement ?? "bottom"} disableHoverListener={!shouldToolTip}>
